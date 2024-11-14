330396b3811eb789ac61fc41c2a45b2ed2f43920
nollieundergrob <92156277+nollieundergrob@users.noreply.github.com>
2024-11-14 08:03:37 +0300

    more_metrics

--- app.py
+++ app.py
@@ -9,10 +9,34 @@ socketio = SocketIO(app)
 
 def background_thread():
     while True:
-        time.sleep(0.3)
+        time.sleep(0.5)
+        # Получаем информацию о CPU и памяти
         cpu = psutil.cpu_percent()
         memory = psutil.virtual_memory().percent
-        socketio.emit('update', {'cpu': cpu, 'memory': memory})
+        
+        # Получаем информацию о сетевых интерфейсах
+        net_io = psutil.net_io_counters()
+        net_connections = psutil.net_connections(kind='inet')
+        
+        # Создаем список соединений
+        connections = []
+        for conn in net_connections:
+            connections.append({
+                'local_address': conn.laddr,
+                'remote_address': conn.raddr,
+                'status': conn.status,
+                'pid': conn.pid
+            })
+        
+        # Отправляем обновления через SocketIO
+        socketio.emit('update', {
+            'cpu': cpu,
+            'memory': memory,
+            'bytes_sent': net_io.bytes_sent,
+            'bytes_recv': net_io.bytes_recv,
+            'conn': connections,
+            'lenght_conn':len(connections)
+        })
 
 @app.route('/')
 def index():
